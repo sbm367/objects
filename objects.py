99cda2bafcbc49331949b5132ed4b9ff29fd3e70
sbm367 <sbm367@protonmail.com>
2021-02-25 16:40:33 -0500

    added sample class

--- objects.py
+++ objects.py
@@ -1,1 +1,21 @@
-print('hello world')
+#Sample class
+
+class PartyAnimal:
+    x = 0
+
+    def party(self):
+        if self.x < 100:
+            print ('so far',self.x)
+            self.x +=1
+            try:
+                self.x = self.party()+1
+            except:
+                pass
+            #self.x = self.x + 1
+        else:
+            print ('so far',self.x)
+
+
+an = PartyAnimal()
+
+an.party()
